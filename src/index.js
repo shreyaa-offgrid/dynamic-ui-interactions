5f8fa88b4f4a4a5b2702cf5d83753bc2eb30f0d0
Shreya Singh <143871325+shreyaa-offgrid@users.noreply.github.com>
2025-12-27 15:16:11 +0000

    Create dropdowns and add styling

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 import "./styles.css";
 
+function makeDropDown(item,btn,dropdown) {
+    btn.addEventListener("mouseover", () => {
+        dropdown.style.display = "flex";
+    });
+    item.addEventListener("mouseleave", () => {
+        dropdown.style.display = "none";
+    });
+}
+
+const academics = document.querySelector(".academics");
+const academicBtn = academics.querySelector(".btn");
+const academicOptns = academics.querySelector(".dropdown");
+makeDropDown(academics,academicBtn,academicOptns);
+
+const fiction = document.querySelector(".fiction");
+const fictionBtn = fiction.querySelector(".btn");
+const fictionOptns = fiction.querySelector(".dropdown");
+makeDropDown(fiction,fictionBtn,fictionOptns);
+
+const spirit = document.querySelector(".spirit");
+const spiritBtn = spirit.querySelector(".btn");
+const spiritOptns = spirit.querySelector(".dropdown");
+makeDropDown(spirit,spiritBtn,spiritOptns);
+
 
