611850aff2833c083f73b86c7a23ef9fd1d24458
Shreya Singh <143871325+shreyaa-offgrid@users.noreply.github.com>
2025-12-27 18:56:53 +0000

    Make image carousel

--- src/index.js
+++ src/index.js
@@ -24,4 +24,42 @@ const spiritBtn = spirit.querySelector(".btn");
 const spiritOptns = spirit.querySelector(".dropdown");
 makeDropDown(spirit,spiritBtn,spiritOptns);
 
+let timer = setInterval(()=>{next.click();},5000);
+const slides = [...document.querySelectorAll(".slide")];
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+const circles = [...document.querySelectorAll(".circle")];
+prev.addEventListener("click",()=>{
+    let activeIdx = getActiveIdx();
+    let nxt = activeIdx-1;
+    if(nxt<0) nxt = slides.length-1;
+    activateSlide(activeIdx,nxt);
+});
+next.addEventListener("click",()=>{
+    let activeIdx = getActiveIdx();
+    let nxt = activeIdx+1;
+    if(nxt>=slides.length) nxt = 0;
+    activateSlide(activeIdx,nxt);
+});
+function getActiveIdx(){
+    return slides.indexOf(document.querySelector(".active"));
+}
+function activateSlide(old,nxt){
+    slides[old].classList.remove("active");
+    slides[nxt].classList.add("active");
+    circles[old].classList.remove("active");
+    circles[nxt].classList.add("active");
+    clearInterval(timer);
+    timer = setInterval(()=>{next.click();},5000);
+}
+for(let idx = 0;idx<circles.length;idx++){
+    circles[idx].addEventListener("click",()=>{
+        let active = getActiveIdx();
+        activateSlide(active,idx);
+    });
+}
+
+
+
+
 
